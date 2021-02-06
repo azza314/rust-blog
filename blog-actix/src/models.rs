912981d9b20c1ef546217b52c021fa102ae04976
Andrea  Piazza <apiazza023@gmail.com>
2021-02-05 22:09:42 -0700

    wip

---
+++ blog-actix/src/models.rs
@@ -0,0 +1,12 @@
+use crate::errors::AppError; 
+use crate::schema::{users};
+use diesel::prelude::*;
+
+type Result<t> = std::result::Result<T, AppError>;
+
+#[derive(Queryable, Identifiable, Serialize, Debug, PartialEq)]
+
+pub struct User{
+    pub id: i32, 
+    pub username: String, 
+}
