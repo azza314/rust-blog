240447f04b2fbd919014ea38da205ea0c96fe265
Andrea  Piazza <apiazza023@gmail.com>
2021-02-11 23:18:45 -0700

    share buffer for wasm and JS

---
+++ wasm/target/debug/deps/wasm.d
@@ -0,0 +1,5 @@
+/Users/andrea/rust/wasm/target/debug/deps/libwasm.dylib: src/lib.rs
+
+/Users/andrea/rust/wasm/target/debug/deps/wasm.d: src/lib.rs
+
+src/lib.rs:
---
+++ wasm/target/debug/libwasm.d
@@ -0,0 +1,1 @@
+/Users/andrea/rust/wasm/target/debug/libwasm.dylib: /Users/andrea/rust/wasm/src/lib.rs
